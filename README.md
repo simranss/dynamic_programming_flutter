cc3c2a420c4affde59f794ddfead2fa7e723c991
simran <simran.s.sharma00@gmail.com>
2022-12-29 22:23:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dynamic_programming_flutter
+
+A new Flutter project that is for designing dynamic UI according to json files.
---
+++ android/app/src/main/kotlin/com/example/dynamic_programming_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dynamic_programming_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
